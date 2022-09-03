f06a42ee0375662e0a258b61ea6c70969033d6e3
Adithya <adithyapantangi123@gmail.com>
2022-09-03 18:35:16 +0530

    Added dummy messages

--- index.js
+++ index.js
@@ -36,25 +36,25 @@ wss.on("connection", function connection(ws) {
   ws.on("close", () => {
     console.log("Connection closed");
   });
+  // });
+
+  let count = 0;
+
+  const interval = setInterval(() => {
+    const message = {
+      user: faker.name.firstName(),
+      userId: nanoid(),
+      message: faker.random.words(),
+      messageId: nanoid(),
+    };
+    ws.send(JSON.stringify(message));
+    count += 1;
+    if (count >= 20) {
+      clearInterval(interval);
+    }
+    return;
+  }, 1000);
 });
-
-let count = 0;
-
-// const interval = setInterval(() => {
-//   const message = {
-//     user: faker.name.firstName(),
-//     userId: nanoid(),
-//     message: faker.random.words(),
-//     messageId: nanoid(),
-//   };
-//   ws.send(JSON.stringify(message));
-//   count += 1;
-//   if (count >= 20) {
-//     clearInterval(interval);
-//   }
-//   return;
-// }, 1000);
-// });
 server.listen(port, () => {
   console.log(`Listening on ws://localhost:${port}`);
 });
